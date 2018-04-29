caceb8cf121a1154957c6b277b01a47c28b2d234
Sujay Kundu <sujaykundu777@gmail.com>
2018-04-30 01:46:08 +0530

    some changes

--- server/server.js
+++ server/server.js
@@ -109,7 +109,7 @@ app.use('/api',index);
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
